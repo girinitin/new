71b70749a77357d9454564d4cde00f3213e98210
tahiralvi <engtahiralvi@gmail.com>
2019-01-08 09:00:55 +0500

    Add an Azure MVC Core Project
    Add an Azure MVC Core Project

---
+++ TaAzureCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaAzureCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
